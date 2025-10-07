1ef3cde9cda24bcfee4afdfa4a33e9ae82f4d53c
SebAlmestro <sebalmestro@gmail.com>
2025-10-07 15:22:17 -0300

    navegar entre bandas

--- templates/bandas/lista_bandas.php
+++ templates/bandas/lista_bandas.php
@@ -1,7 +1,8 @@
 <?php include './templates/header.php'; ?>
+
 <head>
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
-<link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css">
 </head>
 
 <?php include './templates/bandas/form_alta_bandas.php'; ?>
@@ -15,15 +16,17 @@
             <?php foreach ($bandas as $banda): ?>
                 <div class="col-12 col-md-6 mb-4 d-flex justify-content-center">
                     <div class="card shadow-lg border-0" style="width: 100%; max-width: 350px; transition: 0.3s;">
-                        <img src="<?= $banda->Imagen ?>"
-                            class="card-img-top"
-                            alt="<?= $banda->Nombre ?>"
-                            style="height: 220px; object-fit: cover;">
-                        <div class="card-body text-center">
-                            <h5 class="card-title"><?= $banda->Nombre ?></h5>
-                            <p class="card-text mb-2"><strong>Género:</strong> <?= $banda->Genero ?></p>
-                            <p class="card-text"><strong>País:</strong> <?= $banda->Pais_origen ?></p>
-                        </div>
+                        <a href="banda/<?= $banda->id_banda ?>" style="text-decoration: none; color: inherit;">
+                            <img src="<?= $banda->Imagen ?>"
+                                class="card-img-top"
+                                alt="<?= $banda->Nombre ?>"
+                                style="height: 220px; object-fit: cover;">
+                            <div class="card-body text-center">
+                                <h5 class="card-title"><?= $banda->Nombre ?></h5>
+                                <p class="card-text mb-2"><strong>Género:</strong> <?= $banda->Genero ?></p>
+                                <p class="card-text"><strong>País:</strong> <?= $banda->Pais_origen ?></p>
+                            </div>
+                        </a>
                     </div>
                 </div>
             <?php endforeach; ?>
