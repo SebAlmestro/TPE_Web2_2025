d0041fc0bd542172d1f04690c9ca621b975a901b
SebAlmestro <sebalmestro@gmail.com>
2025-10-10 20:06:40 -0300

    Update banda.controller.php

--- app/controllers/banda.controller.php
+++ app/controllers/banda.controller.php
@@ -2,23 +2,27 @@
 require_once './app/models/banda.model.php';
 require_once './app/views/banda.view.php';
 
-class BandaController {
+class BandaController
+{
     private $model;
     private $view;
 
-    function __construct() {
+    function __construct()
+    {
         $this->model = new BandaModel();
         $this->view = new BandaView();
     }
-    
-    function showBandas() {
+
+    function showBandas()
+    {
         // pido las tareas al modelo
         $bandas = $this->model->getBandas();
 
         // se las mando a la vista
         $this->view->showBandas($bandas);
     }
-    function showBanda($id) {
+    function showBanda($id)
+    {
         $banda = $this->model->getBanda($id);
 
         if (!$banda) {
@@ -30,12 +34,17 @@ class BandaController {
     }
 
 
-    function showAgregarBanda() {
+    function showAgregarBanda()
+    {
         $this->view->showAgregarBanda();
     }
 
-    function addBanda() {
-        if (!empty($_POST['nombre']) && !empty($_POST['pais']) && !empty($_POST['genero']) && !empty($_POST['imagen'])) {
+    function addBanda()
+    {
+        if (
+            !empty($_POST['nombre']) && !empty($_POST['pais']) && !empty($_POST['genero'])
+            && !empty($_POST['imagen'])
+        ) {
             $nombre = $_POST['nombre'];
             $pais = $_POST['pais'];
             $genero = $_POST['genero'];
@@ -47,8 +56,9 @@ class BandaController {
             $this->view->showError("Todos los campos son obligatorios");
         }
     }
-    function deleteBanda($id){
-        
+    function deleteBanda($id)
+    {
+
         $banda = $this->model->getBanda($id);
         if (!$banda) {
             return $this->view->showError("No existe la tarea con el id=$id");
@@ -60,11 +70,32 @@ class BandaController {
         header('Location: ' . BASE_URL);
     }
 
-    function editarBanda($id){
+    function showEditarBanda($id)
+    {
         $banda = $this->model->getBanda($id);
 
+        if (!$banda) {
+            $this->view->showError("⚠️ No se encontró la banda con ID $id");
+            return;
+        }
 
-        $this->model->editarBanda($id);
+        $this->view->showEditarBanda($banda);
     }
-
+    function editarBanda($id)
+    {
+        $bandaActual = $this->model->getBanda($id);
+        if ( $_SERVER['REQUEST_METHOD'] === "POST" ) {
+            $nombre = !empty($_POST['nombre']) ? $_POST['nombre'] : $bandaActual->Nombre;
+            $pais   = !empty($_POST['pais']) ? $_POST['pais'] : $bandaActual->Pais_origen;
+            $genero = !empty($_POST['genero']) ? $_POST['genero'] : $bandaActual->Genero;
+            $imagen = !empty($_POST['imagen']) ? $_POST['imagen'] : $bandaActual->Imagen;
+            $this->model->editarBanda($id, $nombre, $pais, $genero, $imagen);
+            header("Location: " . BASE_URL . "bandas");
+        } else {
+            $this->view->showError("Error al editar la banda");
+        }
+        //forma de hacer el if de forma mas reducida,
+        //si lo que viene por post en nombre no esta vacio, entonces
+        //guardo nombre de la banda que ya estaba accediendo al modelo getbanda;
+    }
 }
